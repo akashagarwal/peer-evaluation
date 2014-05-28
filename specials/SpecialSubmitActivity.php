4be4977c49a87a6e166db4e9ac5b850b026d7a45
Akash Agarwal <akashslg@gmail.com>
2014-05-28 17:47:51 +0000

    Database layer added

--- specials/SpecialSubmitActivity.php
+++ specials/SpecialSubmitActivity.php
@@ -18,11 +18,49 @@ class SpecialSubmitActivity extends SpecialPage {
 	 */
 	
 	public function execute( $sub ) {
-		global $wgOut;
+		global $wgOut,$wgUser;
 //		$out = $this->getOutput();
 	
 //		$out->setPageTitle("PeerEvaluation test page");
 
-		$wgOut->addHTML("PeerEvaluation test page");
+//		$wgOut->addHTML("PeerEvaluation test page");
+
+		if (!$wgUser->isLoggedIn())
+		{
+			$wgOut->addHTML("Please Login and return back to this page.");
+			return;
+		}
+
+		$wgOut->setPageTitle("Register Your Blog");
+		$username=$wgUser->getName();
+		
+
+		$userCoursePage="User:".$username."/OCL4Ed 14.02";
+		$title=Title::newFromText($userCoursePage);
+
+		if (!$title->exists())
+		{
+			$wgOut->addHTML("You are not registered for OCL4eD. Please register and come back to this page.");
+			return;
+		}
+		
+			
+//		$wgOut->addHTML("Success: Your course page is it ".$title->getFullURL());
+
+		$wgOut->addHTML("Success<br/>");
+		$dbw = wfGetDB( DB_MASTER );
+
+
+		$dbw->insert(
+			'pe_Activities',
+			array('userid' => $wgUser->getId(), 'URL' => 'dsfjkds', 'Title' => '1', 'Comment' => '1', 'OptedIn' => true, 'Activity_id' => '1'),
+			$fname = 'Database::insert', $options = array()
+			);
+
+
+
+
+			
+
 	}
 }
