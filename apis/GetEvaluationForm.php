673b43823afeff8d4eac71f937fc1cd26d3ab518
Akash Agarwal <akashslg@gmail.com>
2014-06-23 03:18:05 +0530

    Form validiation updated and type 2 in ApiGetEvaluationForm added

--- apis/GetEvaluationForm.php
+++ apis/GetEvaluationForm.php
@@ -55,7 +55,7 @@ class apiGetEvaluationForm extends ApiQueryBase {
         $data.='<b> URL : </b> <a href="'.$activity->URL.'" target="_blank">'.$activity->URL . '</a><br>';
         $data.='<iframe  width="100%"" height="400" src="'.$activity->URL.'"></iframe><br>';
         $data.='If you cannot see the blog post above please try <a href="'.$activity->URL.'" target="_blank"> clicking here</a>. If you cannot find the specified blog post, kindly choose No in the option to the below question and write a comment. <br>';
-        
+
         if ( $activityCd->type == 1 ) {
 
             $questions = $dbr->select(
@@ -94,14 +94,65 @@ class apiGetEvaluationForm extends ApiQueryBase {
             $form .= '
                 </div>
                 <input type="hidden" id="actid" value="'.$id.'">                
-                <input type="submit" value="Submit" id="submitform">
+                <input type="button" value="Submit" id="submitform">
+                </form>
+            ';
+            $data.=$form;
+        }
+
+        if ( $activityCd->type == 2 ) {
+
+            $questions = $dbr->select(
+                'pe_questions_10point',
+                array( '*'),
+                $conds = 'activity_id= ' . $activityId,
+                $fname = __METHOD__,
+                $options = array( '' )
+            );
+
+            $form ='
+                <form>
+                Is the content of the post related to ' . $activityCd->title . ' ?
+                <input type="radio" name="Related" value="1" class="Related">Yes
+                <input type="radio" name="Related" value="0" class="Related">No <br>
+                <p>Comment (optional)</p>
+                <textarea name="Related_comment" id="Related_comment" rows="2" cols="50"></textarea>
+                <br>
+                <div id="formcontent">
+            ';
+
+            foreach ( $questions as $row ) {
+                $form.='<div id="ques" name="'.$row->id.'">';
+                $form.=$row->Question . '<br>';
+                $no=$row->No_options;
+                $form.='<input type="radio" name="'.$row->id.'" value="Not achieved">Not achieved';
+                $form.='<input type="radio" name="'.$row->id.'" value="Achieved">Achieved';
+                $form.='<input type="radio" name="'.$row->id.'" value="Merit">Merit';
+
+                $form.='<span id="rNot achieved'.$row->id.'" > "'.$row->Notachieved_rubric.'" <br> ';
+                $form.= '</span>';
+
+                $form.='<span id="rcontent"></span>';
+                $form.='<br><p>Comment (optional)</p>
+                <textarea id="c'.$row->id.'" rows="2" cols="50"></textarea>
+                <br>';
+
+                $form.='</div><br>';
+            }
+            
+            $form .= '
+                </div>
+                <input type="hidden" id="actid" value="'.$id.'">                
+                <input type="button" value="Submit" id="submitform">
                 </form>
             ';
             $data.=$form;
         }
 
 
+
         $result->addValue(null, $this->getModuleName(),array('success' => $data));
+        $result->addValue(null, $this->getModuleName(),array('type' => $activityCd->type));
 
 
         return true;
